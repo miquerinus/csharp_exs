74c17b85348574eee58437346c6f5a39845684b6
miquerinus <miquerinus.br@gmail.com>
2024-03-26 17:28:56 -0300

    alterações net7 utilizado no projeto

--- obj/Debug/net7.0/proj01.AssemblyInfo.cs
+++ obj/Debug/net7.0/proj01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proj01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f73036eafeaf93fc3bb4567db31bddefcfd105")]
 [assembly: System.Reflection.AssemblyProductAttribute("proj01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proj01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
